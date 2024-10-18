847236c7959fdd973685019e0a87d7c67cb57119
Ayinla12 <95141742+Ayinla12@users.noreply.github.com>
2024-10-18 11:53:56 +0100

    Update google.java

--- src/test/java/testngPackage/google.java
+++ src/test/java/testngPackage/google.java
@@ -43,6 +43,7 @@ public class google {
         driver.get("https://www.amazon.com/");
         System.out.println("amazon is opened successfully");
         System.out.println("amazon is awesome");
+        //abc
 
     }
 }
